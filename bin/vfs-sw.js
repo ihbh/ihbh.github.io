b078478353f07e9205b71ab67371b95b8d415a85
ihbh <ihbh+github@protonmail.com>
2019-10-14 23:18:18 -0700

    Lazy-load json-fs.

--- bin/vfs-sw.js
+++ bin/vfs-sw.js
@@ -1,37 +1,42 @@
-define(["require", "exports", "./pwa", "./json-fs", "./prop"], function (require, exports, pwa, json_fs_1, prop_1) {
+define(["require", "exports", "./prop"], function (require, exports, prop_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     const CACHE_DIR = '/cache';
-    let jsonfs = new json_fs_1.default({
-        keys: new prop_1.AsyncProp({
-            nocache: true,
-            async get() {
-                let keys = await pwa.invoke('cache.keys');
-                return keys.map(encodeURIComponent);
+    let jsonfs = new prop_1.AsyncProp(async () => {
+        let { default: JsonFS } = await new Promise((resolve_1, reject_1) => { require(['./json-fs'], resolve_1, reject_1); });
+        let pwa = await new Promise((resolve_2, reject_2) => { require(['./pwa'], resolve_2, reject_2); });
+        return new JsonFS({
+            keys: new prop_1.AsyncProp({
+                nocache: true,
+                async get() {
+                    let keys = await pwa.invoke('cache.keys');
+                    return keys.map(encodeURIComponent);
+                },
+            }),
+            read: async (key) => {
+                let url = decodeURIComponent(key);
+                return pwa.invoke('cache.read', { url });
             },
-        }),
-        read: async (key) => {
-            let url = decodeURIComponent(key);
-            return pwa.invoke('cache.read', { url });
-        },
-        parseKey: key => {
-            let url = decodeURIComponent(key);
-            let i = url.indexOf('://');
-            let j = url.indexOf('/', i < 0 ? 0 : i + 3);
-            if (j < 0)
-                return [url];
-            let schema = url.slice(0, i);
-            let domain = url.slice(i + 3, j);
-            let path = url.slice(j + 1);
-            return [schema, domain, ...path.split('/')]
-                .map(encodeURIComponent);
-        },
+            parseKey: key => {
+                let url = decodeURIComponent(key);
+                let i = url.indexOf('://');
+                let j = url.indexOf('/', i < 0 ? 0 : i + 3);
+                if (j < 0)
+                    return [url];
+                let schema = url.slice(0, i);
+                let domain = url.slice(i + 3, j);
+                let path = url.slice(j + 1);
+                return [schema, domain, ...path.split('/')]
+                    .map(encodeURIComponent);
+            },
+        });
     });
     exports.default = new class {
         async get(path) {
             if (path.startsWith(CACHE_DIR)) {
-                let relpath = path.slice(CACHE_DIR.length) || '/';
-                return jsonfs.get(relpath);
+                let rel = path.slice(CACHE_DIR.length) || '/';
+                let fs = await jsonfs.get();
+                return fs.get(rel);
             }
             throw new Error('Cannot dir: ' + path);
         }
@@ -39,14 +44,16 @@ define(["require", "exports", "./pwa", "./json-fs", "./prop"], function (require
             if (path === '/')
                 return [CACHE_DIR.slice(1)];
             if (path.startsWith(CACHE_DIR)) {
-                let relpath = path.slice(CACHE_DIR.length) || '/';
-                return jsonfs.dir(relpath);
+                let rel = path.slice(CACHE_DIR.length) || '/';
+                let fs = await jsonfs.get();
+                return fs.dir(rel);
             }
             throw new Error('Cannot dir: ' + path);
         }
         async rmdir(path) {
             if (path != CACHE_DIR)
                 throw new Error('Cannot rmdir: ' + path);
+            let pwa = await new Promise((resolve_3, reject_3) => { require(['./pwa'], resolve_3, reject_3); });
             return pwa.invoke('cache.clear');
         }
     };
--- src/vfs-sw.ts
+++ src/vfs-sw.ts
@@ -1,40 +1,44 @@
-import * as pwa from './pwa';
 import { VFS } from './vfs-api';
-import JsonFS from './json-fs';
 import { AsyncProp } from './prop';
 
 const CACHE_DIR = '/cache';
 
-let jsonfs = new JsonFS({
-  keys: new AsyncProp({
-    nocache: true,
-    async get() {
-      let keys: string[] = await pwa.invoke('cache.keys');
-      return keys.map(encodeURIComponent);
+let jsonfs = new AsyncProp(async () => {
+  let { default: JsonFS } = await import('./json-fs');
+  let pwa = await import('./pwa');
+
+  return new JsonFS({
+    keys: new AsyncProp({
+      nocache: true,
+      async get() {
+        let keys: string[] = await pwa.invoke('cache.keys');
+        return keys.map(encodeURIComponent);
+      },
+    }),
+    read: async key => {
+      let url = decodeURIComponent(key);
+      return pwa.invoke('cache.read', { url });
     },
-  }),
-  read: async key => {
-    let url = decodeURIComponent(key);
-    return pwa.invoke('cache.read', { url });
-  },
-  parseKey: key => {
-    let url = decodeURIComponent(key);
-    let i = url.indexOf('://');
-    let j = url.indexOf('/', i < 0 ? 0 : i + 3);
-    if (j < 0) return [url];
-    let schema = url.slice(0, i);
-    let domain = url.slice(i + 3, j);
-    let path = url.slice(j + 1);
-    return [schema, domain, ...path.split('/')]
-      .map(encodeURIComponent);
-  },
+    parseKey: key => {
+      let url = decodeURIComponent(key);
+      let i = url.indexOf('://');
+      let j = url.indexOf('/', i < 0 ? 0 : i + 3);
+      if (j < 0) return [url];
+      let schema = url.slice(0, i);
+      let domain = url.slice(i + 3, j);
+      let path = url.slice(j + 1);
+      return [schema, domain, ...path.split('/')]
+        .map(encodeURIComponent);
+    },
+  });
 });
 
 export default new class implements VFS {
   async get(path: string) {
     if (path.startsWith(CACHE_DIR)) {
-      let relpath = path.slice(CACHE_DIR.length) || '/';
-      return jsonfs.get(relpath);
+      let rel = path.slice(CACHE_DIR.length) || '/';
+      let fs = await jsonfs.get();
+      return fs.get(rel);
     }
 
     throw new Error('Cannot dir: ' + path);
@@ -45,8 +49,9 @@ export default new class implements VFS {
       return [CACHE_DIR.slice(1)];
 
     if (path.startsWith(CACHE_DIR)) {
-      let relpath = path.slice(CACHE_DIR.length) || '/';
-      return jsonfs.dir(relpath);
+      let rel = path.slice(CACHE_DIR.length) || '/';
+      let fs = await jsonfs.get();
+      return fs.dir(rel);
     }
 
     throw new Error('Cannot dir: ' + path);
@@ -55,6 +60,7 @@ export default new class implements VFS {
   async rmdir(path: string) {
     if (path != CACHE_DIR)
       throw new Error('Cannot rmdir: ' + path);
+    let pwa = await import('./pwa');
     return pwa.invoke('cache.clear');
   }
 };
